4d3de149b16354c30235683c60e3f1be7d8eaed4
wset2 <wtaisen@gmail.com>
2020-12-02 23:32:29 +0000

    feat: Day1_2 solution and modified day 1 code

--- 1/day1_1.scala
+++ 1/day1_1.scala
@@ -1,12 +1,6 @@
 import scala.io._
 
-object Day1 {
-    // def multiplyEntries(report: Array[Int]) : Int = {
-    //     var leftovers = report.map(x => 2020 - x)
-    //     var intersection = List(report, leftovers).reduce((a, b) => a intersect b)
-    //     println(intersection)
-    // }
-
+object Day1_1 {
     def main(args: Array[String]) {
         var target: Int = 2020
         var finalNumber: Int = 1
